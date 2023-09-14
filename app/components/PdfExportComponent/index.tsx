3b94a832ac43b0bf93af99d4fdb2efce51a93daf
djcranney <danieljamescranney@gmail.com>
2023-09-14 19:59:33 +0100

    Assignment Options

--- app/components/PdfExportComponent/index.tsx
+++ app/components/PdfExportComponent/index.tsx
@@ -17,28 +17,32 @@ const assignments = [
     label: "Written Assignment",
     options: [
       {
-        value: `Formatted the file name and document title correctly?`,
+        value: `Formatted the file name and document title correctly`,
         additionalInfo: `eg: 
 DM4001/001 - Essay - James Dixon`,
+        alternativeValue: `I have correctly formatted the file name and document title.`,
       },
       {
-        value: "Formatted with font size 12 using a standard sans-serif font?",
+        value: "Formatted with font size 12 using a standard sans-serif font",
         additionalInfo: "eg: Arial, Helvetica, Montserrat.",
+        alternativeValue: `I have correctly formatted the text so it size 12 and a standard sans-serif font.`,
       },
       {
-        value: "Double-spaced text?",
+        value: "Double-spaced text",
         additionalInfo: "",
+        alternativeValue: `I have double-spaced text.`,
       },
       {
         value:
-          "Cited sources in-text and in the bibliography, using the Harvard Referencing?",
+          "Cited sources in-text and in the bibliography, using the Harvard Referencing",
         additionalInfo: "eg: Barker, D. (2013) Book Title. Routledge: Reading",
+        alternativeValue: `I have used the Harvard Referencing System to cite sources.`,
       },
       {
         value:
-          "Have you proof-read your assignment to identify and remove errors?",
-        additionalInfo:
-          "I have proof-read your assignment to identify and remove errors.",
+          "Have you proof-read your assignment to identify and remove errors",
+        additionalInfo: "",
+        alternativeValue: `I have proof-read this assignment to identify and remove errors.`,
       },
     ],
   },
@@ -47,15 +51,31 @@ DM4001/001 - Essay - James Dixon`,
     label: "Practical Assignment",
     options: [
       {
-        value: `Have you formatted file names and document titles correctly? eg: 
+        value: `Have you formatted file names and document titles correctly?`,
+        additionalInfo: `eg: 
 DM4001/001 - Essay - James Dixon`,
-        additionalInfo: "I correctly formatted file names and document titles.",
+        alternativeValue: `I have correctly formatted the file names and document titles.`,
       },
       {
-        value: `Have you used appropriate file types? Eg: Video (MP4, MPEG, MOV), Audio (MP3, WAV), Graphics (PSD, JPG, PNG)`,
-        alternativeValue: `I have used appropriate file types, eg: Video (MP4, MPEG, MOV), Audio (MP3, WAV), Graphics (PSD, JPG, PNG)`,
+        value: `Have you used appropriate file types?`,
+        additionalInfo: `Eg: Video (MP4, MPEG, MOV), Audio (MP3, WAV), Graphics (PSD, JPG, PNG)`,
+        alternativeValue: `I have used appropriate file types for this assignment.`,
       },
-      // Add more options as needed
+      {
+        value: `Standard aspect ratio?`,
+        additionalInfo: `Eg: 4:3, 16:9, 9:16 or 1:1`,
+      },
+      {
+        value: `Have you watched the exported file through?`,
+        additionalInfo:
+          "This will help you avoid basic technical issues like black-screen or silenced audio.",
+        alternativeValue: `I have watched the exported file through before submitting it.`,
+      },
+      {
+        value: `Have you included evidence of an iterative process (ie previous versions)?`,
+        additionalInfo: "This is essential for accessing higher grades.",
+        alternativeValue: `I have included evidence of an iterative process.`,
+      },
     ],
   },
   {
@@ -63,29 +83,22 @@ DM4001/001 - Essay - James Dixon`,
     label: "Presentation Assignment",
     options: [
       {
-        value: "Option 1",
-        additionalInfo: "Alternate Value 1",
+        value: `Formatted the file name and document title correctly`,
+        additionalInfo: `eg: 
+DM5001/001 - Presentation - James Dixon`,
+        alternativeValue: `I have correctly formatted the file name and document title.`,
       },
       {
-        value: "Option 2",
-        additionalInfo: "Alternate Value 2",
-      },
-      // Add more options as needed
-    ],
-  },
-  {
-    type: "other",
-    label: "Other Assignment",
-    options: [
-      {
-        value: "Option 1",
-        additionalInfo: "Alternate Value 1",
+        value:
+          "Cited sources in-text and in the bibliography, using the Harvard Referencing",
+        additionalInfo: "eg: Barker, D. (2013) Book Title. Routledge: Reading",
+        alternativeValue: `I have used the Harvard Referencing System to cite sources.`,
       },
       {
-        value: "Option 2",
-        additionalInfo: "Alternate Value 2",
+        value: `Is your presentation video in an appropriate file type?`,
+        additionalInfo: `Eg: Video (MP4, MPEG, MOV)`,
+        alternativeValue: `I have uploaded my presentation in an appropriate file type.`,
       },
-      // Add more options as needed
     ],
   },
 ];
@@ -138,19 +151,6 @@ const PdfExportComponent: React.FC = () => {
               maiores. Ducimus consequatur ex dolor labore reiciendis in
               repudiandae quis porro minus et! Itaque.
             </Text>
-            <Svg
-              style={styles.logo}
-              width="1936.000000pt"
-              height="1989.000000pt"
-              viewBox="0 0 1936.000000 1989.000000"
-              preserveAspectRatio="xMidYMid meet"
-            >
-              <G
-                transform="translate(0.000000,1989.000000) scale(0.100000,-0.100000)"
-                fill="#000000"
-                stroke="none"
-              ></G>
-            </Svg>
 
             {selectedCheckboxes.map((checkboxValue, index) => (
               <Text key={index}>{checkboxValue}</Text>
@@ -237,34 +237,42 @@ const PdfExportComponent: React.FC = () => {
         className={`flex flex-col gap-y-2 p-6 bg-gradient-to-br from-white/[4%] to-transparent mix-blend-screen rounded-lg transition-[height] h-auto mb-6`}
       >
         <h3 className="text-2xl font-extrabold text-white">Checklist</h3>
-        <p className="text-sm font-bold">Select an option from below.</p>
 
         {assessmentType === "" ? (
-          <p>Please select an assessment type</p>
+          <p className="text-sm font-bold">Select an assessment type above.</p>
         ) : (
-          assignments.map((assignment) => {
-            if (assignment.type === assessmentType) {
-              return (
-                <div key={assignment.type}>
-                  {assignment.options.map((option) => (
-                    <label
-                      key={option.value}
-                      className="flex w-full py-1 text-white text-base font-bold gap-x-1.5"
-                    >
-                      <input
-                        type="checkbox"
-                        value={option.value}
-                        onChange={handleCheckboxChange}
-                        checked={selectedCheckboxes.includes(option.value)}
-                      />
-                      {option.value}
-                    </label>
-                  ))}
-                </div>
-              );
-            }
-            return null;
-          })
+          <>
+            {assignments.map((assignment) => {
+              if (assignment.type === assessmentType) {
+                return (
+                  <div key={assignment.type} className="gap-y-1 flex flex-col">
+                    {assignment.options.map((option) => (
+                      <label
+                        key={option.value}
+                        className="flex w-full py-1 text-white text-base font-bold gap-x-1.5"
+                      >
+                        <input
+                          type="checkbox"
+                          value={option.value}
+                          onChange={handleCheckboxChange}
+                          checked={selectedCheckboxes.includes(option.value)}
+                        />
+                        <div className="flex flex-col">
+                          <span className="text-base font-bold">
+                            {option.value}
+                          </span>
+                          <span className="text-xs font-medium opacity-90">
+                            {option.additionalInfo}
+                          </span>
+                        </div>
+                      </label>
+                    ))}
+                  </div>
+                );
+              }
+              return null;
+            })}
+          </>
         )}
       </article>
 
@@ -295,7 +303,7 @@ const PdfExportComponent: React.FC = () => {
                   <path d="M7 11l5 5l5 -5"></path>
                   <path d="M12 4l0 12"></path>
                 </svg>
-                Loading
+                Download PDF
               </button>
             ) : (
               <button className="flex items-center bg-gradient-to-tr hover:from-[#3a334f] hover:to-[#be35b0] text-white font-bold text-base rounded-lg px-3.5 py-3 from-[#3a334f]/80 to-[#be35b0]/80 transition-all duration-150 ease-in-out hover:scale-[102%] gap-x-1">
